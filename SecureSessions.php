2b1403c321b5a8cab6cea9cd17b2733d52f2f86e
Reedy <reedy@wikimedia.org>
2017-02-10 17:33:11 +0000

    Convert SecureSessions to extension registration
    Bug: T98668
    Change-Id: Ifaeaff591d68ecc01c9be4a0e757d82dc0ed6e21

--- SecureSessions.php
+++ SecureSessions.php
@@ -1,71 +1,15 @@
 <?php
 
-/**
- * SecureSessions extension for MediaWiki
- * (c) 2012 Tyler Romeo <tylerromeo@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @file
- * @link https://www.mediawiki.org/wiki/Extension:SecureSessions
- * @ingroup Extensions
- * @author Tyler Romeo <tylerromeo@gmail.com>
- * @copyright (c) 2012 by Tyler Romeo
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0 or later
- */
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'SecureSessions',
-	'version' => '0.6.0',
-	'author' => 'Tyler Romeo',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SecureSessions',
-	'descriptionmsg' => 'securesessions-desc'
-);
-
-/**
- * Set options for what properties to check on each user request. The value
- * should be an array with zero or more of the following keys:
- *  * 'ip' - Restrict session to an IP address
- *  * 'useragent' - Restrict session to the User Agent header
- *  * 'onesession' - When a user logs in, log out all other sessions
- * For each of these keys, it should be assigned to one of the following values:
- *  * true - Force this restriction on all users
- *  * null - Allow the user to decide on login
- *  * false - Disable this restriction
- * If this is set to the boolean value true, it is assumed to be an array
- * containing all of the above values. If it is set to the boolean value false,
- * it is assumed to be an empty array.
- */
-$wgEnhancedSessionAuth = true;
-
-/**
- * Whether to cycle the session ID on every request. There may be some performance
- * implications if your wiki is not using memcached sessions.
- */
-$wgSessionCycleId = false;
-
-// Setup for the extension.
-$wgAutoloadClasses['SecureSessions'] = __DIR__ . '/SecureSessions.hooks.php';
-$wgAutoloadClasses['SpecialSessions'] = __DIR__ . '/SpecialSessions.php';
-$wgExtensionFunctions[] = array( 'SecureSessions', 'setup' );
-$wgSpecialPages['Sessions'] = 'SpecialSessions';
-$wgMessagesDirs['SecureSessions'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['SecureSessions'] = __DIR__ . '/SecureSessions.i18n.php';
-
-if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once __DIR__ . '/vendor/autoload.php';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SecureSessions' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['SecureSessions'] = __DIR__ . '/i18n';
+	/*wfWarn(
+		'Deprecated PHP entry point used for SecureSessions extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);*/
+	return;
+} else {
+	die( 'This version of the SecureSessions extension requires MediaWiki 1.28+' );
 }
