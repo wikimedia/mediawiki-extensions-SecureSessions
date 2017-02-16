7989077fe5780f231dd137877cfc54ab3786414d
Umherirrender <umherirrender_de.wp@web.de>
2017-02-16 20:50:10 +0100

    Load existing alias file
    Add it to ExtensionMessagesFiles in extension.json
    
    Change-Id: I62812c46d1560775ec1b73e3ff2e2e1c44355c7d

--- SecureSessions.php
+++ SecureSessions.php
@@ -4,6 +4,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SecureSessions' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SecureSessions'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['SecureSessionsAlias'] = __DIR__ . '/SpecialSessions.alias.php';
 	/*wfWarn(
 		'Deprecated PHP entry point used for SecureSessions extension. ' .
 		'Please use wfLoadExtension instead, ' .
